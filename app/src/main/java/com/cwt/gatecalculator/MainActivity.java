bd856d8bf9a644e8d37abb2b96e6309cf3ca6055
phonix007 <telegramtutorial007@gmail.com>
2021-08-04 09:45:31 +0530

    Done Now Cal

--- app/src/main/java/com/cwt/gatecalculator/MainActivity.java
+++ app/src/main/java/com/cwt/gatecalculator/MainActivity.java
@@ -2,13 +2,28 @@ package com.cwt.gatecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_str = findViewById(R.id.str_btn);
+
+        btn_str.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent categoryIntent = new Intent(MainActivity.this, Calculator.class);
+                startActivity(categoryIntent);
+            }
+        });
     }
 }
