2035dd4e8b5502627ee0673a0ee56c661131cc5d
phonix007 <telegramtutorial007@gmail.com>
2021-08-04 13:31:54 +0530

    1raw

--- app/src/main/java/com/cwt/gatecalculator/Calculator.java
+++ app/src/main/java/com/cwt/gatecalculator/Calculator.java
@@ -2,6 +2,7 @@ package com.cwt.gatecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class Calculator extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
